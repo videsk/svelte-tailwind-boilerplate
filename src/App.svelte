a5ab06d1e35d5ec77f6a57ff451ae89104be69f8
Matias Lopez <matiaslopezd@gmail.com>
2020-06-16 20:25:50 -0400

    NEW:First upload

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
